b3455a1083af1d2e199482761f615b474a4912be
linth <zhua0404@gmail.com>
2024-01-25 15:18:24 +0800

    modify and remove comment code.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,13 @@
 import { Controller, Get, Inject, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-// import { MyLogger } from './MyLogger/MyLogger.service';
 
 @Controller()
-export class AppController {
-  // option 1: 使用 new object 方式來建立 logging.
-  // logger: Logger = new Logger(AppController.name, {timestamp: true});
-  // mylogger: MyLogger = new MyLogger();
-  
+export class AppController {  
 
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-
-    // option 1
-    // 'log', 'error', 'warn', 'debug', 'verbose'
-    // this.logger.log('first log information.');
-    // this.logger.error('first error information.');
-    // this.logger.warn('first warn information.');
-    // this.logger.debug('first debug information.');
-    // this.logger.verbose('first verbose information.');
-
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,22 +3,18 @@ import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common';
 @Injectable()
 export class AppService {
   constructor(
-    // option 2: 測試使用依賴注入方式 logging.
+    // 使用依賴注入方式 logging.
     @Inject('UsersLogger')
     private readonly logger: Logger,    
   ) {}
   
   getHello(): string {
-    
     // 'log', 'error', 'warn', 'debug', 'verbose'
     this.logger.log('first DI log information.');
     this.logger.error('first DI error information.');
     this.logger.warn('first DI warn information.');
     this.logger.debug('first DI debug information.');
     this.logger.verbose('first DI verbose information.');
-
-    // TODO: option 3: 增加 custom logging.
-
     return 'Hello World!';
   }
 }
